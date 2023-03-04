0e2aa85c9095fa90f06d5f4f07eaff2299af2539
Andrew Feng <bxandrew@gmail.com>
2023-03-04 15:31:11 -0500

    implement a rough implementation of ChooseMuscle component to choose a muscle group before being able to navigate to the exercise list for the chosen muscle group

--- src/components/modal/workout/ChooseMuscle.jsx
+++ src/components/modal/workout/ChooseMuscle.jsx
@@ -1,7 +1,71 @@
 import React from "react";
+import Grid from "@mui/material/Grid";
+import Card from "@mui/material/Card";
+import CardContent from "@mui/material/CardContent";
+import CardMedia from "@mui/material/CardMedia";
+import Typography from "@mui/material/Typography";
+import { CardActionArea } from "@mui/material";
 
 function ChooseMuscle() {
-  return <div>ChooseMuscle Component</div>;
+  return (
+    <div>
+      <div>Choose Muscle Component</div>
+      <Grid
+        container
+        spacing={0}
+        // sx={{ justifyContent: "center", alignItems: "center" }}
+      >
+        <Grid item xs={4} align="center">
+          <Card
+            sx={{
+              width: 200,
+              height: 200,
+              backgroundColor: "blue",
+            }}
+          >
+            <CardActionArea>
+              <Typography
+                sx={{
+                  width: 200,
+                  height: 200,
+                  display: "flex",
+                  justifyContent: "center",
+                  alignItems: "center",
+                  backgroundColor: "lightblue",
+                }}
+              >
+                Quads
+              </Typography>
+            </CardActionArea>
+          </Card>
+        </Grid>
+        <Grid item xs={4} align="center">
+          <Card
+            sx={{
+              width: 200,
+              height: 200,
+              backgroundColor: "blue",
+            }}
+          >
+            <CardActionArea>
+              <Typography
+                sx={{
+                  width: 200,
+                  height: 200,
+                  display: "flex",
+                  justifyContent: "center",
+                  alignItems: "center",
+                  backgroundColor: "lightblue",
+                }}
+              >
+                Biceps
+              </Typography>
+            </CardActionArea>
+          </Card>
+        </Grid>
+      </Grid>
+    </div>
+  );
 }
 
 export default ChooseMuscle;
