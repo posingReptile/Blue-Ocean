b77afff61e1df2e185c7cf535c4111f13294d5e6
Jesus Solorzano <jesus.solorz@gmail.com>
2023-03-03 19:32:39 -0500

    very basic profile page

--- src/components/profile/profile.jsx
+++ src/components/profile/profile.jsx
@@ -16,11 +16,63 @@ prs
 
 */
 import React from 'react';
+import defaultProfileImage from '../../assets/pfpic.png';
+import Box from '@mui/material/Box';
+import Avatar from '@mui/material/Avatar';
+import Badge from '@mui/material/Badge';
+import EditIcon from '@mui/icons-material/Edit';
+
 // import '../css/profile.css';
+
+// api or props?
 
 function Profile() {
   return (
-    <div>profile page</div>
+    <Box>
+      username
+      <Box>
+        <Badge
+          overlap="circular"
+          anchorOrigin={{ vertical: 'bottom', horizontal: 'right' }}
+          badgeContent={
+            <EditIcon sx={{ color: 'action' }}/>
+          }
+        >
+          <Avatar
+            alt="Travis Howard"
+            src={defaultProfileImage}
+            sx={{
+              width: 99,
+              height: 99
+            }}
+          />
+        </Badge>
+      </Box>
+
+      <Box sx={{ display: 'grid', gridTemplateColumns: 'repeat(3, 1fr)' }}>
+        <Box sx={{ width: '100%' }}>
+          <a>age</a>
+        </Box>
+        <Box sx={{ width: '100%' }}>
+          <a></a>
+        </Box>
+        <Box sx={{ width: '100%' }}>
+          <a>weight</a>
+        </Box>
+
+        <Box sx={{ width: '100%' }}>
+          <a>height</a>
+        </Box>
+        <Box sx={{ width: '100%' }}>
+          <a></a>
+        </Box>
+        <Box sx={{ width: '100%' }}>
+          <a>target weight</a>
+        </Box>
+      </Box>
+
+
+    </Box>
   );
 }
 
