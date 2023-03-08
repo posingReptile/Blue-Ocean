f430641631c1c8f7c1b4ff26dc54cc35237aefb6
Jesus Solorzano <jesus.solorz@gmail.com>
2023-03-08 11:40:43 -0500

    added date picker for message, preliminary axios requests

--- src/components/profile/adminPage.jsx
+++ src/components/profile/adminPage.jsx
@@ -1,23 +1,58 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import {
   Box, FormControl, Paper, Button, TextField, Stack, Typography, Table, TableHead, TableBody, TableRow, TableCell, TableContainer
 } from '@mui/material';
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
+import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
+import { DatePicker } from '@mui/x-date-pickers/DatePicker';
 import KeyboardBackspaceIcon from '@mui/icons-material/KeyboardBackspace';
+
 /*
 app analytics
-  # of users, workouts planned,
   number of calorie goals completed
-  ability to post custom daily messages / announcements
 */
 
 function AdminPage(props) {
   const { goBack } = props;
-  const [bannerMessage, setBannerMessage] = useState('');
+
+  const [messageDate, setMessageDate] = useState(null);
+  const [clicked, setClicked] = useState(false);
+
+  function upDate(event) {
+    const day = (event.$D > 10) ? event.$D : ('0' + event.$D);
+    const month = ((event.$M + 1) > 10) ? (event.$M + 1) : ('0' + (event.$M + 1))
+    const year = event.$y;
+
+    if(new Date(event.$d) < new Date(Date.now())){
+      setMessageDate(null);
+      setClicked(true);
+    } else setMessageDate(year + month + day);
+  }
 
   function submitBannerMessage(event) {
     event.preventDefault();
-    setBannerMessage(event.target.elements.adminMessage.value);
+    if (messageDateError) setClicked(true);
+    else {
+      if (clicked) setClicked(false);
+      axios.post(/admin-message/, {
+        message: event.target.elements.adminMessage.value,
+        date: messageDate
+      })
+        .then(() => console.log('admin message posted'))
+        .catch(() => console.log('failed to post message'));
+    }
   }
+
+  useEffect(() => {
+    axios.get('/admin-users')
+      .then(({data}) => {
+        console.log(data);
+      })
+      .catch(() => console.log('failed to get admin data'));
+  }, []);
+
+  const messageDateError = (messageDate) ? false : true;
 
   return (
     <Box>
@@ -26,11 +61,17 @@ function AdminPage(props) {
       </Button>
       <form onSubmit={submitBannerMessage} style={{ minWidth: '100%' }}>
         <FormControl onSubmit={submitBannerMessage} sx={{ minWidth: '100%' }}>
-            <TextField id="adminMessage" label="new banner message" variant="outlined" />
-            <Box direction="row" sx={{ textAlign: 'right', width: '100%' }}>
-              <Button variant="outlined">remove</Button>
-              <Button variant="contained" type="submit">post</Button>
-            </Box>
+          <Box sx={{ alignItems: 'right', textAlign: 'right' }}>
+            <LocalizationProvider dateAdapter={AdapterDayjs}>
+              <DatePicker label="message date" onChange={upDate} disablePast required />
+            </LocalizationProvider>
+            {messageDateError && clicked && <div className="errorText">Enter a valid date</div>}
+          </Box>
+          <TextField id="adminMessage" label="message" variant="outlined" required />
+          <Box direction="row" sx={{ textAlign: 'right', width: '100%', mt: 1 }}>
+            <Button variant="outlined">remove</Button>
+            <Button variant="contained" type="submit">post</Button>
+          </Box>
         </FormControl>
       </form>
       <TableContainer sx={{ mt: 3 }} component={Paper}>
