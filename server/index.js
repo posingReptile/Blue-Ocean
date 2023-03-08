c0d8b047860423fd53cde255690be4bd8e4b811d
Andrew Feng <bxandrew@gmail.com>
2023-03-08 15:51:01 -0500

    removing a rogue comment.

--- server/index.js
+++ server/index.js
@@ -23,7 +23,7 @@ import dotenv from "dotenv";
 import axios from "axios";
 import cors from "cors";
 import session from "express-session";
-import { fileURLToPath } from 'url';
+import { fileURLToPath } from "url";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
