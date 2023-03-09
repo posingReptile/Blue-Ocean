9c76c0cd9e512e5bee3f2d6fb6b702dffad7b257
Jerry MacPherson <jmpherso@gmail.com>
2023-03-09 11:27:58 -0600

    bugfix from merge

--- src/components/navbar/ResponsiveNavBar.jsx
+++ src/components/navbar/ResponsiveNavBar.jsx
@@ -26,13 +26,8 @@ function ResponsiveAppBar({ userObject, setComponent, setUserObject }) {
   const handleCloseNavMenu = (event, componentName) => {
     setAnchorElNav(null);
     console.log(componentName);
-<<<<<<< HEAD
-    setComponent(componentName);
-    // setComponent(componentName);
-=======
     // setComponent("calendar");
     setComponent(componentName);
->>>>>>> main
   };
 
   const handleOpenUserMenu = (event) => {
@@ -136,7 +131,6 @@ function ResponsiveAppBar({ userObject, setComponent, setUserObject }) {
           >
             <Button
               onClick={(e) => handleCloseNavMenu(e, "calendar")}
-<<<<<<< HEAD
               sx={{
                 my: 2,
                 color: "white",
@@ -147,15 +141,11 @@ function ResponsiveAppBar({ userObject, setComponent, setUserObject }) {
                   color: '#3c52b2',
               },
               }}
-=======
-              sx={{ my: 2, color: "white", display: "block", fontSize: 16 }}
->>>>>>> main
             >
               Calendar
             </Button>
             <Button
               onClick={(e) => handleCloseNavMenu(e, "dashboard")}
-<<<<<<< HEAD
               sx={{
                 my: 2,
                 mr: 2,
@@ -167,9 +157,6 @@ function ResponsiveAppBar({ userObject, setComponent, setUserObject }) {
                   color: '#3c52b2',
               },
               }}
-=======
-              sx={{ my: 2, color: "white", display: "block", fontSize: 16 }}
->>>>>>> main
             >
               Dashboard
             </Button>
